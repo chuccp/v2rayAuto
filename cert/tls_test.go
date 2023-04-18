281aafda01a2e0e684045c1d6c90f54124d0ba0e
tomcat <cooge123@gmail.com>
2023-04-18 22:58:03 +0800

    Update tls_test.go

--- cert/tls_test.go
+++ cert/tls_test.go
@@ -1,24 +1,13 @@
 package cert
 
 import (
-	"fmt"
 	"github.com/chuccp/v2rayAuto/util"
-	"github.com/v2fly/v2ray-core/v5/common/protocol/tls/cert"
 	"log"
 	"testing"
 )
 
 func TestCert(t *testing.T) {
-	cs, err := GetCertificateFromLego([]string{"ws.cooge.top"}, "cooge123@gmail.com")
-	if err != nil {
-		log.Fatalln("obtains certificate:", err)
-	}
-	cert, err := cert.ParseCertificate(cs.Certificate, cs.PrivateKey)
-	if err != nil {
 
-	}
-	certPEM, keyPEM := cert.ToPEM()
-	fmt.Println(certPEM, keyPEM)
 }
 func TestSaveCert(t *testing.T) {
 
@@ -45,13 +34,5 @@ func TestSaveCert(t *testing.T) {
 	}
 }
 func TestCert222(t *testing.T) {
-
-	pem, key, err := LoadCertPem("127.0.0.1", "cooge123@gmail.com", "", 80)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-	t.Log(pem)
-	t.Log(key)
 
 }
